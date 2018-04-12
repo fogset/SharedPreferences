4f483abb150681dd2df35f7029ff4c806af0e6a0
Tianhao <zhangth1990@hotmail.com>
2018-04-11 23:57:07 -0600

    added a new array

--- app/src/main/java/com/example/tianhao/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/tianhao/sharedpreferences/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,8 +17,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.tianhao.sharedpreferences", Context.MODE_PRIVATE);
-        sharedPreferences.edit().putString("username", "nick").apply();
-        String username = sharedPreferences.getString("username", "");
-        Log.i("This is the username", username);
+
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("Fido");
+        friends.add("Sarah");
+        friends.add("Jones");
+        
+        try {
+            sharedPreferences.edit().putString("friends",ObjectSerializer.serialize(friends)).apply();
+            Log.i("friends", ObjectSerializer.serialize(friends));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> newFriends = new ArrayList<>();
+        try {
+            newFriends = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("friends", ObjectSerializer.serialize(new ArrayList<String>())));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        Log.i("new Friends", newFriends.toString());
+        //sharedPreferences.edit().putString("username", "nick").apply();
+        //String username = sharedPreferences.getString("username", "");
+        //Log.i("This is the username", username);
     }
 }
